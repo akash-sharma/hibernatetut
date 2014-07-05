c638315b02109e76d6ce1fb086ede10c8334273c
akash-sharma <akash777.sharma@gmail.com>
2014-07-05 19:49:14 +0530

    adding comments for one to one mappings

--- src/main/java/com/akash/hibernate/hibernatetut/onetoone/App.java
+++ src/main/java/com/akash/hibernate/hibernatetut/onetoone/App.java
@@ -7,29 +7,3 @@ public class App
 		new OneToOneMapping().operationOnOneToOne();
 	}
 }
-
-//Caused by: org.hibernate.MappingException: Could not determine type for: com.akash.hibernate.hibernatetut.onetoone.Vehicle, at table: Person, for columns: [org.hibernate.mapping.Column(vehicle)]
-
-//Exception in thread "main" org.hibernate.TransientObjectException: object references an unsaved transient instance - save the transient instance before flushing: com.akash.hibernate.hibernatetut.onetoone.Vehicle
-
-/**
- * <<Saving Person with Vehicle>>
- * 
- * Hibernate: insert into Person (personName, vehicle_id) values (?, ?)
- * Hibernate: insert into Vehicle (vehicleName) values (?)
- * Hibernate: update Person set personName=?, vehicle_id=? where id=?
- * 
- * 
- * <<Saving Person without Vehicle>>
- * 
- * Hibernate: insert into Person (personName, vehicle_id) values (?, ?)
- * 
- * 
- * NOTE:
- * When we apply OnToOne mapping from A to B, the B is Nullable true in class A.
- * 
- * NOTE:
- * When we apply OneToOne mapping from A to B and we delete object of A,
- * then object of B is not deleted automatically.
- * 
- */
--- src/main/java/com/akash/hibernate/hibernatetut/onetoone/OneToOneMapping.java
+++ src/main/java/com/akash/hibernate/hibernatetut/onetoone/OneToOneMapping.java
@@ -46,3 +46,35 @@ public class OneToOneMapping
         session.getTransaction().commit();
 	}
 }
+
+//Caused by: org.hibernate.MappingException: Could not determine type for: com.akash.hibernate.hibernatetut.onetoone.Vehicle, at table: Person, for columns: [org.hibernate.mapping.Column(vehicle)]
+
+//Exception in thread "main" org.hibernate.TransientObjectException: object references an unsaved transient instance - save the transient instance before flushing: com.akash.hibernate.hibernatetut.onetoone.Vehicle
+
+/**
+* <<Saving Person with Vehicle>>
+* 
+* Hibernate: insert into Person (personName, vehicle_id) values (?, ?)
+* Hibernate: insert into Vehicle (vehicleName) values (?)
+* Hibernate: update Person set personName=?, vehicle_id=? where id=?
+* 
+* 
+* <<Saving Person without Vehicle>>
+* 
+* Hibernate: insert into Person (personName, vehicle_id) values (?, ?)
+* 
+* 
+* NOTE:
+* (1)Let say we have 2 Entity classes A and B and
+* 		we create object of B as instance variable in A
+* 		without any mapping, then MappingException will occur.
+* 
+* (2) When we apply OnToOne mapping from A to B, 
+* 		the B is Nullable true in class A.
+* 
+* (3) When we apply OneToOne mapping from A to B and
+* 		we delete object of A,
+* 		then object of B is not deleted automatically.
+* 
+* 
+*/
