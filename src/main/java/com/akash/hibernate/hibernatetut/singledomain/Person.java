65b7bbb6a1d8cbfc7ba1620f9cfcfa14234c5a32
akash-sharma <akash777.sharma@gmail.com>
2014-09-07 23:53:27 +0530

    documentation added

--- src/main/java/com/akash/hibernate/hibernatetut/singledomain/Person.java
+++ src/main/java/com/akash/hibernate/hibernatetut/singledomain/Person.java
@@ -10,6 +10,22 @@ import javax.persistence.Table;
 @Table(name="person_table")
 public class Person
 {
+/**
+ * (1) Acc to hibernate, any data that has to be saved in DB must have an annotation of either
+ *     @Entity or @Embedded on its class level.
+ * (2) An Entity class must have an @Id annotation on a field.Entity class must have a primary key.
+ * (3) @GeneratedValue annotation provides the way values of Id field is generated. 
+ * (4) @Table annotation specifies the name of the table at DB level and 
+ *     @Entity specifies the name of the entity at hibernate level.
+ * (5) @Column attribute is used to configure column of a table.
+ *     Each data type of java class has a corresponding default data type in DB.
+ *     We can also configure this type in @Column annotation.
+ * (6) Any persistent object when get updated , will automatically be persisted to DB.
+ * (7) Difference between session.load() and session.get()
+ * (8) Session class => save() , saveOrUpdate() , merge() , update()
+ * (9) EntityManager class => merge() , find() , persist() , flush()  
+ */
+	
 	@Id @GeneratedValue @Column(name="person_id")
 	int personId;
 	
