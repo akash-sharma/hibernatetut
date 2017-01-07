1b1130adc93624836debfd3250aac68fd1407ac0
akash-sharma <akash777.sharma@gmail.com>
2017-01-07 23:01:27 +0530

    adding jpa layer and tested NamedEntityGraph

--- src/main/java/com/akash/hibernate/hibernatetut/onetomany/OneToManyJpaImpl.java
+++ src/main/java/com/akash/hibernate/hibernatetut/onetomany/OneToManyJpaImpl.java
@@ -17,10 +17,26 @@ public class OneToManyJpaImpl {
 	public static void main(String args[]) throws Exception {
 		createOneToMany();
 		cloneHuman();
-		//cloneHumanUsingNeg();
+		// cloneHumanUsingNeg();
 		JpaUtil.close();
 	}
 
+	/**
+	 * 1 sql queries is fired <br>
+	 * 
+	 * Hibernate: select human0_.id as id1_4_0_, vehicle2_.id as id1_27_1_,
+	 * tyre4_.id as id1_22_2_, human0_.name as name2_4_0_, vehicle2_.name as
+	 * name2_27_1_, vehiclelis1_.Human_id as Human_id1_4_0__,
+	 * vehiclelis1_.vehicleList_id as vehicleList_id2_5_0__, tyre4_.name as
+	 * name2_22_2_, tyres3_.Vehicle_onetomany_id as Vehicle_onetomany_1_27_1__,
+	 * tyres3_.tyres_id as tyres_id2_28_1__ from Human human0_ left outer join
+	 * Human_Vehicle_onetomany vehiclelis1_ on human0_.id=vehiclelis1_.Human_id
+	 * left outer join Vehicle_onetomany vehicle2_ on
+	 * vehiclelis1_.vehicleList_id=vehicle2_.id left outer join
+	 * Vehicle_onetomany_Tyre tyres3_ on
+	 * vehicle2_.id=tyres3_.Vehicle_onetomany_id left outer join Tyre tyre4_ on
+	 * tyres3_.tyres_id=tyre4_.id where human0_.id=1 before clone after clone
+	 */
 	private static void cloneHumanUsingNeg() throws Exception {
 		EntityManager entityManager = JpaUtil.getEntitymanager();
 		entityManager.getTransaction().begin();
@@ -54,17 +70,24 @@ public class OneToManyJpaImpl {
 	}
 
 	/**
-	 * 3 sql queries are fired after human is loaded <br>
-	 * 1 for vehicle <br>
+	 * 4 sql queries are fired <br>
+	 * 1 for human <br>
+	 * 1 for all vehicle linked to human <br>
 	 * 2 for tyres of each vehicle <br>
 	 * 
 	 * Hibernate: select human0_.id as id1_4_, human0_.name as name2_4_ from
+	 * Human human0_ where human0_.id=1 <br>
+	 * before clone
+	 * 
+	 * Hibernate: select human0_.id as id1_4_, human0_.name as name2_4_ from
 	 * Human human0_ where human0_.id=1 before clone Hibernate: select
-	 * vehiclelis0_.Human_id as Human_id1_4_0_, vehiclelis0_.vehicleList_id as
-	 * vehicleList_id2_5_0_, vehicle1_.id as id1_27_1_, vehicle1_.name as
-	 * name2_27_1_ from Human_Vehicle_onetomany vehiclelis0_ inner join
-	 * Vehicle_onetomany vehicle1_ on vehiclelis0_.vehicleList_id=vehicle1_.id
-	 * where vehiclelis0_.Human_id=?
+	 * human0_.id as id1_4_, human0_.name as name2_4_ from Human human0_ where
+	 * human0_.id=1 before clone Hibernate: select vehiclelis0_.Human_id as
+	 * Human_id1_4_0_, vehiclelis0_.vehicleList_id as vehicleList_id2_5_0_,
+	 * vehicle1_.id as id1_27_1_, vehicle1_.name as name2_27_1_ from
+	 * Human_Vehicle_onetomany vehiclelis0_ inner join Vehicle_onetomany
+	 * vehicle1_ on vehiclelis0_.vehicleList_id=vehicle1_.id where
+	 * vehiclelis0_.Human_id=?
 	 * 
 	 * Hibernate: select tyres0_.Vehicle_onetomany_id as
 	 * Vehicle_onetomany_1_27_0_, tyres0_.tyres_id as tyres_id2_28_0_, tyre1_.id
@@ -77,6 +100,8 @@ public class OneToManyJpaImpl {
 	 * as id1_22_1_, tyre1_.name as name2_22_1_ from Vehicle_onetomany_Tyre
 	 * tyres0_ inner join Tyre tyre1_ on tyres0_.tyres_id=tyre1_.id where
 	 * tyres0_.Vehicle_onetomany_id=? after clone
+	 * 
+	 * after clone
 	 */
 
 	private static void createOneToMany() {
